c46ed19e2e50637e9628d61212a3b94e6512592f
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-02-03 21:35:47 +0530

    ➕Add keybind: searching git files & Remove Alpha Plugin

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sF", builtin.git_files, { desc = "[S]earch Git [F]iles" })
       vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
       vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
