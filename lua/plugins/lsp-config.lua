6f8b6364953bbf0a458d729c355955bd7dbef511
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-03-03 18:16:19 +0530

    wip: rewrite lsp part

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,29 +24,33 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			local capabilities =
-					require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-			local lspconfig = require("lspconfig")
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "[G]oto [d]efinition" })
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" })
-			vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { desc = "[G]oto [T]ype Definition" })
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "[G]oto [I]mplementation" })
-			vim.keymap.set("n", '<leader>D', require('telescope.builtin').lsp_type_definitions, { desc = 'Type [D]efinition' })
-			vim.keymap.set("n", '<leader>ds', require('telescope.builtin').lsp_document_symbols,
-				{ desc = '[D]ocument [S]ymbols' })
-			vim.keymap.set("n", '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-				{ desc = '[W]orkspace [S]ymbols' })
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
-			-- lsp mention
-			lspconfig.lua_ls.setup({ capabilities = capabilities, filetype = { "lua" } })
-			lspconfig.pyright.setup({ capabilities = capabilities, filetype = { "python" } })
-			lspconfig.bashls.setup({ capabilities = capabilities, filetype = { "sh", "bash" } })
-			lspconfig.docker_compose_language_service.setup({ capabilities = capabilities, filetype = { "yml", "yaml" } })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.dockerls.setup({ capabilities = capabilities, filetype = { "dockerfile" } })
-			lspconfig.ast_grep.setup({ capabilities = capabilities })
-			lspconfig.intelephense.setup({ capabilities = capabilities })
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local map = function(key, func, desc)
+				vim.keymap.set("n", key, func, { desc = desc })
+			end
+			map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+			map("K", vim.lsp.buf.hover, "Hover Documentation")
+			map("gd", vim.lsp.buf.definition, "[G]oto [d]efinition")
+			map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+			map("gT", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+			map("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+			map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+			map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+			map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+			-- lsp mentions
+			local lspname = function(name, filetypes)
+				require("lspconfig")[name].setup({ capabilities = capabilities, filetype = filetypes })
+			end
+			lspname("lua_ls", { "lua" })
+			lspname("pyright", { "python" })
+			lspname("bashls", { "sh", "bash" })
+			lspname("docker_compose_language_service", { "yml", "yaml" })
+			lspname("clangd", { "c", "cpp" })
+			lspname("dockerls", { "dockerfile" })
+			lspname("ast_grep", { "javascript", "typescript" })
+			lspname("intelephense", { "php" })
 		end,
 	},
 }
