1714e683cafd306495226f1bc3e5b7a191d797ee
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-03-03 22:49:24 +0530

    wip: rewrite lspconfig

--- lua/plugins/lint-format.lua
+++ lua/plugins/lint-format.lua
@@ -24,11 +24,12 @@ return {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.black,
 					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.shfmt,
 					null_ls.builtins.formatting.prettier,
 					null_ls.builtins.formatting.clang_format,
 					-- diagnostics
 					null_ls.builtins.diagnostics.phpcs,
-					null_ls.builtins.diagnostics.pylint,
+					null_ls.builtins.diagnostics.mypy,
 					null_ls.builtins.diagnostics.stylelint,
 				},
 			})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,56 +1,45 @@
 return {
-	{
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
 		"williamboman/mason.nvim",
-		dependencies = {
-			{ "j-hui/fidget.nvim", opts = {} },
-		},
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
 		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pylsp", "pyright" },
-			})
-		end,
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		{ "j-hui/fidget.nvim", opts = {} },
 	},
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local capabilities =
-				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-			local map = function(key, func, desc)
-				vim.keymap.set("n", key, func, { desc = desc })
-			end
-			map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-			map("K", vim.lsp.buf.hover, "Hover Documentation")
-			map("gd", vim.lsp.buf.definition, "[G]oto [d]efinition")
-			map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-			map("gT", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
-			map("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-			map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-			map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-			map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-			map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+		local map = function(key, func, desc)
+			vim.keymap.set("n", key, func, { desc = desc })
+		end
+		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+		map("K", vim.lsp.buf.hover, "Hover Documentation")
+		map("gd", vim.lsp.buf.definition, "[G]oto [d]efinition")
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+		map("gT", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+		map("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+		map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+		map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-			-- lsp mentions
-			local lspname = function(name, filetypes)
-				require("lspconfig")[name].setup({ capabilities = capabilities, filetype = filetypes })
-			end
-			lspname("lua_ls", { "lua" })
-			lspname("pyright", { "python" })
-			lspname("bashls", { "sh", "bash" })
-			lspname("docker_compose_language_service", { "yml", "yaml" })
-			lspname("clangd", { "c", "cpp" })
-			lspname("dockerls", { "dockerfile" })
-			lspname("ast_grep", { "javascript", "typescript" })
-			lspname("intelephense", { "php" })
-		end,
-	},
+		-- lsp mentions
+		local lspname = function(name, filetypes)
+			require("lspconfig")[name].setup({ capabilities = capabilities, filetype = filetypes })
+		end
+		lspname("lua_ls", { "lua" })
+		lspname("pyright", { "python" })
+		lspname("bashls", { "sh", "bash" })
+		lspname("docker_compose_language_service", { "yml", "yaml" })
+		lspname("clangd", { "c", "cpp" })
+		lspname("dockerls", { "dockerfile" })
+		lspname("html", { "html" })
+		lspname("tsserver", { "javascript", "typescript" })
+		lspname("cssls", { "css" })
+		lspname("intelephense", { "php" })
+
+		require("mason").setup()
+		require("mason-tool-installer").setup({ ensure_installed = { "stylua", "lua_ls", "pylsp", "pyright" } })
+		require("mason-lspconfig").setup({ opts = { auto_install = true } })
+	end,
 }
