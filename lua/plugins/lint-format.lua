dd1d1c95b9148842f90e0fb795c8ba35b598e325
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-03-03 17:38:06 +0530

    wip: get it working

--- lua/plugins/lint-format.lua
+++ lua/plugins/lint-format.lua
@@ -20,7 +20,16 @@ return {
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
+					-- formatting
 					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.clang_format,
+					-- diagnostics
+					null_ls.builtins.diagnostics.phpcs,
+					null_ls.builtins.diagnostics.pylint,
+					null_ls.builtins.diagnostics.stylelint,
 				},
 			})
 			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "[G]et [F]ormat" })
