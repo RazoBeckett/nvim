b1118c9f7abcc7c6916de7d2b2e77dba73faaced
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-04-11 12:06:01 +0530

    feat: configure autocmds
    - add autocmd for xresource auto reloading
    - make sh file executable

---
+++ lua/razobeckett/autocmds.lua
@@ -0,0 +1,13 @@
+-- xresources file auto reload
+vim.api.nvim_create_autocmd({ "BufWritePost" }, { pattern = "*Xresources", command = "!xrdb -load %" })
+-- make shell scripts executable
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "sh",
+	callback = function()
+		-- check if the file is executable
+		if vim.fn.executable(vim.fn.expand("%:p")) == 0 then
+			vim.fn.system("chmod +x " .. vim.fn.expand("%"))
+			vim.notify("Script is now executable", "info")
+		end
+	end,
+})
--- lua/razobeckett/init.lua
+++ lua/razobeckett/init.lua
@@ -1,6 +1,7 @@
 require("razobeckett.keymaps")
 require("razobeckett.options-globals")
 require("razobeckett.lazy")
+require("razobeckett.autocmds")
 
 vim.cmd.hi("Comment cterm=italic gui=italic")
 vim.api.nvim_create_autocmd("TextYankPost", {
