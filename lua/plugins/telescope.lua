7b9e60e127c72f5bd7794d3a74be10562f8d9272
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-04-19 19:45:01 +0530

    feat: plugin vim-tmux-navigator
    install vim-tmux-navigator

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,8 +21,6 @@ return {
 				path_display = { "smart" },
 				mappings = {
 					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 				},
