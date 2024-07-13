7d5b6cdd9dcd953b0824d664d8871736f886875f
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-07-13 17:48:19 +0530

    change keybinds of harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,9 +5,9 @@ return {
 	},
 	keys = {
 		{ "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>",        desc = "Mark file with harpoon" },
-		{ "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<CR>",          desc = "Go to next harpoon mark" },
-		{ "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<CR>",          desc = "Go to previous harpoon mark" },
-		{ "<leader>he", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Toggle the harpoon quick menu" },
+		{ "<C-S-l>", "<cmd>lua require('harpoon.ui').nav_next()<CR>",             desc = "Go to next harpoon mark" },
+		{ "<C-S-h>", "<cmd>lua require('harpoon.ui').nav_prev()<CR>",             desc = "Go to previous harpoon mark" },
+		{ "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",      desc = "Toggle the harpoon quick menu" },
 	},
 	config = function()
 		for i = 1, 5 do
