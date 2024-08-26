f09100a48b5f98f6a07ac700f91e63bab5508a0c
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-08-26 22:41:58 +0530

    feat: new colorscheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,35 +9,17 @@ return {
 		end,
 	},
 	{
-		"RRethy/nvim-base16",
-		priority = 1000,
-		lazy = false,
+		-- "razobeckett/uwunified.nvim",
+		dir = "/home/razobeckett/Developer/personal/uwunified.nvim",
 		dependencies = {
+			--- for trnasparent background
 			"xiyaowong/transparent.nvim",
 		},
 		config = function()
-			require("base16-colorscheme").with_config({
-				telescope = false,
+			require("transparent").setup({
+				exclude_groups = { "CursorLine" },
 			})
-			require("base16-colorscheme").setup({
-				-- uwunicorn
-				base00 = "#242b26",
-				base01 = "#2f2a3f",
-				base02 = "#46354a",
-				base03 = "#6c3c62",
-				base04 = "#7e5f83",
-				base05 = "#eed5d9",
-				base06 = "#d9c2c6",
-				base07 = "#e4ccd0",
-				base08 = "#de5b44",
-				base09 = "#e39755",
-				base0A = "#a84a73",
-				base0B = "#c965bf",
-				base0C = "#9c5fce",
-				base0D = "#6a9eb5",
-				base0E = "#6ac38f",
-				base0F = "#a3ab5a",
-			})
+			vim.cmd.colorscheme("uwunified")
 		end,
 	},
 	{
