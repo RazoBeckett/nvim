5fa5b2ff4ea1cb231e62572ff9edc207a2f810ac
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-08-28 15:25:54 +0530

    style: Format keybindings
    Adjust the formatting of keybindings in harpoon.lua for
    better readability and consistency in code style

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,9 +2,9 @@ return {
 	"ThePrimeagen/harpoon",
 	keys = {
 		{ "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>",        desc = "Mark file with harpoon" },
-		{ "<C-S-l>", "<cmd>lua require('harpoon.ui').nav_next()<CR>",             desc = "Go to next harpoon mark" },
-		{ "<C-S-h>", "<cmd>lua require('harpoon.ui').nav_prev()<CR>",             desc = "Go to previous harpoon mark" },
-		{ "<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",      desc = "Toggle the harpoon quick menu" },
+		{ "<C-S-l>",    "<cmd>lua require('harpoon.ui').nav_next()<CR>",          desc = "Go to next harpoon mark" },
+		{ "<C-S-h>",    "<cmd>lua require('harpoon.ui').nav_prev()<CR>",          desc = "Go to previous harpoon mark" },
+		{ "<C-e>",      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Toggle the harpoon quick menu" },
 	},
 	config = function()
 		for i = 1, 5 do
