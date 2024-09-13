d631b5989deb514d2a2dbca64a135f42fef72baf
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-09-13 20:45:00 +0530

    feat(.MD): Note-taking exp. improvments

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,19 +1,27 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
-	lazy = true,
-	ft = "markdown",
-	config = function()
-		require("obsidian").setup({
-			workspaces = {
-				{
-					name = "Notes",
-					path = "~/Documents/Notes",
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = "markdown",
+		config = function()
+			require("obsidian").setup({
+				workspaces = {
+					{
+						name = "Notes",
+						path = "~/Documents/Notes",
+					},
 				},
-			},
-			picker = {
-				name = "telescope.nvim",
-			},
-		})
-	end,
+				picker = {
+					name = "telescope.nvim",
+				},
+				ui = {
+					enable = false,
+				},
+			})
+		end,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+	},
 }
