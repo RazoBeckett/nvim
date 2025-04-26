9fa8193fe0406678d3217d1b0091a0b992644490
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-04-26 23:25:00 +0530

    fix(lsp): update deprecated API usage enhance diagnostic config and styling

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -35,13 +35,34 @@ return {
 		})
 		local original_capabilities = vim.lsp.protocol.make_client_capabilities()
 		-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-		capabilities = require('blink.cmp').get_lsp_capabilities(original_capabilities)
+		capabilities = require("blink.cmp").get_lsp_capabilities(original_capabilities)
 
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			severity_sort = true,
+			float = { border = "rounded", source = "if_many" },
+			underline = { severity = vim.diagnostic.severity.ERROR },
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = "󰋽 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+				},
+			},
+			virtual_text = {
+				source = "if_many",
+				spacing = 2,
+				format = function(diagnostic)
+					local diagnostic_message = {
+						[vim.diagnostic.severity.ERROR] = diagnostic.message,
+						[vim.diagnostic.severity.WARN] = diagnostic.message,
+						-- [vim.diagnostic.severity.INFO] = diagnostic.message,
+						-- [vim.diagnostic.severity.HINT] = diagnostic.message,
+					}
+					return diagnostic_message[diagnostic.severity]
+				end,
+			},
+		})
 
 		mason_lspconfig.setup_handlers({
 			function(server_name)
