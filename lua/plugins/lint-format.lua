32b270d43519ae41947429c8de1a17fa6670e1fd
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-04-26 23:20:05 +0530

    chore(lint-format): remove nvim-cmp dependency from autopairs config

--- lua/plugins/lint-format.lua
+++ lua/plugins/lint-format.lua
@@ -51,9 +51,6 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		event = { "InsertEnter" },
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-		},
 		config = function()
 			local autopairs = require("nvim-autopairs")
 			autopairs.setup({
@@ -65,9 +62,6 @@ return {
 					java = false,
 				},
 			})
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
 }
