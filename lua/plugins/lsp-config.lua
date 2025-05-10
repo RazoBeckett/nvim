b4d93bd258d443d00ed2e80c1b4014b22bcad62a
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-05-10 22:52:15 +0530

    feat(lsp): update LSP configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,41 +2,60 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		{ "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+		-- Mason core & Mason-LSPConfig bridge
+		{ "williamboman/mason.nvim", version = "^1.0.0" },
+		{ "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
+
+		-- Completion engine replacement
+		{ "saghen/blink.cmp", dependencies = { "rafamadriz/friendly-snippets" } },
+
+		-- Optional helpers
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
 		{ "j-hui/fidget.nvim", opts = {} },
-		{ "mason-org/mason.nvim", version = "^1.0.0" },
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
-		local util = require("lspconfig/util")
+		-- Bootstrap Mason
+		local mason = require("mason")
+		mason.setup()
+
+		-- Bridge Mason -> lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
-
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local map = function(key, func, desc)
-					vim.keymap.set("n", key, func, { buffer = ev.buf, silent = true, desc = desc })
-				end
-				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-				map("K", vim.lsp.buf.hover, "Hover Documentation")
-				map("gd", vim.lsp.buf.definition, "[G]oto [d]efinition")
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-				map("gT", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
-				map("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-				map("gr", vim.lsp.buf.references, "[G]et [R]eferences")
-				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-			end,
+		mason_lspconfig.setup({
+			ensure_installed = { "lua_ls", "ansiblels", "gopls" },
+			automatic_installation = true,
 		})
-		local original_capabilities = vim.lsp.protocol.make_client_capabilities()
-		-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-		capabilities = require("blink.cmp").get_lsp_capabilities(original_capabilities)
 
+		-- Shared on_attach & capabilities
+		local blink_cmp = require("blink.cmp")
+		blink_cmp.setup()
+		local capabilities = blink_cmp.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+		local on_attach = function(client, bufnr)
+			local bufmap = function(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
+			end
+			bufmap("n", "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+			bufmap("n", "K", vim.lsp.buf.hover, "Hover Documentation")
+			bufmap("n", "gd", vim.lsp.buf.definition, "[G]oto [d]efinition")
+			bufmap("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+			bufmap("n", "gT", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+			bufmap("n", "gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+			bufmap("n", "gr", vim.lsp.buf.references, "[G]et [R]eferences")
+			bufmap("n", "<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			bufmap("n", "<leader>D", function()
+				require("telescope.builtin").lsp_type_definitions()
+			end, "Type [D]efinition")
+			bufmap("n", "<leader>ds", function()
+				require("telescope.builtin").lsp_document_symbols()
+			end, "[D]ocument [S]ymbols")
+			bufmap("n", "<leader>ws", function()
+				require("telescope.builtin").lsp_dynamic_workspace_symbols()
+			end, "[W]orkspace [S]ymbols")
+		end
+
+		-- Global diagnostics config
 		vim.diagnostic.config({
 			severity_sort = true,
 			float = { border = "rounded", source = "if_many" },
@@ -52,44 +71,65 @@ return {
 			virtual_text = {
 				source = "if_many",
 				spacing = 2,
-				format = function(diagnostic)
-					local diagnostic_message = {
-						[vim.diagnostic.severity.ERROR] = diagnostic.message,
-						[vim.diagnostic.severity.WARN] = diagnostic.message,
-						-- [vim.diagnostic.severity.INFO] = diagnostic.message,
-						-- [vim.diagnostic.severity.HINT] = diagnostic.message,
-					}
-					return diagnostic_message[diagnostic.severity]
+				format = function(d)
+					if d.severity <= vim.diagnostic.severity.WARN then
+						return d.message
+					end
 				end,
 			},
 		})
 
-		lspconfig.lua_ls.setup({
-			diagnostics = {
-				global = { "vim" },
-			},
-			completion = {
-				callSnippets = "Replace",
-			},
-		})
-		lspconfig.ansiblels.setup({
-			cmd = { "ansible-language-server", "--stdio" },
-			filetypes = { "yaml", "ansible" },
-			init_options = {
-				extraVars = {
-					ansible = {
-						ansible = "ansible",
-						ansiblePlaybook = "ansible-playbook",
-						ansibleGalaxy = "ansible-galaxy",
-						ansibleLint = "ansible-lint",
+		-- Auto-setup all installed servers with optional overrides
+		mason_lspconfig.setup_handlers({
+			-- Default handler
+			function(server_name)
+				require("lspconfig")[server_name].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end,
+
+			-- lua_ls override
+			["lua_ls"] = function()
+				require("lspconfig").lua_ls.setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+							completion = { callSnippet = "Replace" },
+						},
 					},
-				},
-			},
-		})
-		lspconfig.gopls.setup({
-			cmd = { "gopls" },
-			filetypes = { "go", "gomod", "gowork", "gotmpl" },
-			root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+				})
+			end,
+
+			-- ansiblels override
+			["ansiblels"] = function()
+				require("lspconfig").ansiblels.setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+					cmd = { "ansible-language-server", "--stdio" },
+					filetypes = { "yaml", "ansible" },
+					init_options = {
+						ansible = {
+							ansible = "ansible",
+							ansiblePlaybook = "ansible-playbook",
+							ansibleLint = "ansible-lint",
+						},
+					},
+				})
+			end,
+
+			-- gopls override
+			["gopls"] = function()
+				require("lspconfig").gopls.setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+					cmd = { "gopls" },
+					filetypes = { "go", "gomod", "gowork", "gotmpl" },
+					root_dir = require("lspconfig/util").root_pattern("go.work", "go.mod", ".git"),
+				})
+			end,
 		})
 	end,
 }
