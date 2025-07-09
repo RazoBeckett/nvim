4d7e640f88cf358041600e19be3f9b062e5f5888
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-07-09 20:03:23 +0530

    feat(tj): floating terminal

--- lua/razobeckett/init.lua
+++ lua/razobeckett/init.lua
@@ -3,6 +3,7 @@ require("razobeckett.keymaps")
 require("razobeckett.options-globals")
 require("razobeckett.lazy")
 require("razobeckett.autocmds")
+require("razobeckett.floating-terminal")
 
 vim.cmd.hi("Comment cterm=italic gui=italic")
 vim.api.nvim_create_autocmd("TextYankPost", {
