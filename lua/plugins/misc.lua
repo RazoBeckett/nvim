77770e0f4f2033c5f52a8aac465dbb5561b62f9f
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-07-09 19:53:45 +0530

    feat(plugin): REST Client

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -198,4 +198,18 @@ return {
 			})
 		end,
 	},
+	{
+		"mistweaverco/kulala.nvim",
+		keys = {
+			{ "<leader>Rs", desc = "Send request" },
+			{ "<leader>Ra", desc = "Send all requests" },
+			{ "<leader>Rb", desc = "Open scratchpad" },
+		},
+		ft = { "http", "rest" },
+		opts = {
+			global_keymaps = true,
+			global_keymaps_prefix = "<leader>R",
+			kulala_keymaps_prefix = "",
+		},
+	},
 }
