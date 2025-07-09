ec3cad91de9ed8c8d370739f85b9ed160e6393b6
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-07-09 20:01:07 +0530

    feat(format): auto format on save

--- lua/plugins/lint-format.lua
+++ lua/plugins/lint-format.lua
@@ -44,6 +44,13 @@ return {
 					nix = { "alejandra" },
 				},
 			})
+			-- Autoformat on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
 			vim.keymap.set("n", "<leader>gf", function()
 				require("conform").format({ bufnr = 0, async = true, lsp_format = "fallback" })
 			end, { desc = "[G]et [F]ormat" })
